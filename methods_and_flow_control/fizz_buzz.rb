f60985b69b4c401b7773e2d42c947ac06754da02
Laurie Linz <lvlinz@comcast.net>
2016-02-27 17:31:49 -0700

    Tried once again for the desired result of on number divisable by both 5 and 3 to only print fizzbuzz.
    [#114169639]

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -3,10 +3,10 @@
 1.upto(100).each do |x|
   fizz = x % 3 == 0
   buzz = x % 5 == 0
-  fizzbuzz = x % 3 && x % 5
+  fizzbuzz = x % 3 == 0 && x % 5 == 0
 
-  puts 'Fizz' if fizz || !fizzbuzz
-  puts 'Buzz' if buzz || !fizzbuzz
+  puts 'Fizz' if fizz && !fizzbuzz
+  puts 'Buzz' if buzz && !fizzbuzz
   puts 'FizzBuzz' if fizz && buzz
   puts x if !fizz && !buzz
 end
