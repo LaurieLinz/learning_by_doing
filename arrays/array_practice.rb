10a3722a24120c466e88118adf3665a970fc4927
Laurie Linz <lvlinz@comcast.net>
2016-02-29 17:52:35 -0700

    WIP
    [#114456621]

---
+++ arrays/array_practice.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+array.each do |number|
+  print "#{number}..."
+end
+
+print "T-"
+array.each do |number|
+  print "#{number},"
+end
+
