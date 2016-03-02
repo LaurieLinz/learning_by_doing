2cdd4bab18aa9aacde705b92ee32a371a32d1f71
Laurie Linz <lvlinz@comcast.net>
2016-03-01 19:53:54 -0700

    added last verse
    [#114455549]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,8 +1,20 @@
 #!/usr/bin/env ruby
 
-99.downto(1) do |beer|
+99.downto(2) do |beer|
   puts "#{beer} bottles of beer on the wall, #{beer} bottles of beer.
 Take one down and pass it around, #{beer - 1} bottles of beer on the wall.
 
 "
 end
+
+puts "2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."
+
+
+
