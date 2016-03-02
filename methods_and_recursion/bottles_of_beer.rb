c9285f611ac9246531fd2703d0d7de4095cb43e4
Laurie Linz <lvlinz@comcast.net>
2016-03-01 19:31:54 -0700

    Removed space per Travis
    [#114455549]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 99.downto(1) do |beer|
-  puts "#{beer} bottles of beer on the wall,  #{beer} bottles of beer.
+  puts "#{beer} bottles of beer on the wall, #{beer} bottles of beer.
 Take one down and pass it around, #{beer - 1} bottles of beer on the wall.
 
 "
