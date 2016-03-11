423b7bc56a9d4fb166344bc9580a1e5260d7f595
Laurie Linz <lvlinz@comcast.net>
2016-03-10 19:15:11 -0700

    Updated homework per TA feedback
    [#114456657]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -3,27 +3,19 @@
 # Instead of using an iterator, put the numbers
 # 1 through 100 in an array called "numbers"
 numbers = (1..100).to_a
+new_array = numbers
 
 # Create another array that has Fizz, Buzz,
 # FizzBuzz or the number for each element of numbers
 
-numbers.map! do |x|
-  fizz = x % 3 == 0
-  buzz = x % 5 == 0
-  fizzbuzz = x % 3 == 0 && x % 5 == 0
-  
+new_array.map! do |x|
+  x = 'fizzbuzz' if x % 3 == 0 && x % 5 == 0
+  x = 'fizz' if x % 3 == 0
+  x = 'buzz' if x % 5 == 0
+  x if x % 5 != 0 || x % 3 != 0
 end
 
 # Print out this array and you should have
 # the same output that fizz_buzz.rb produced.
 
-new_array = []
-numbers << new_array
-
-var = 'Fizz' if fizz && !fizzbuzz
-var = 'Buzz' if buzz && !fizzbuzz
-var = 'FizzBuzz' if fizz && buzz
-var = x if !fizz && !buzz
-
-
-
+puts new_array
