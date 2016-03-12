2d229cdaebd788e38b4b8b245971a6aaa9428889
Laurie Linz <lvlinz@comcast.net>
2016-03-12 16:13:53 -0700

    WIP
    [#115220675]

--- objects_tdd/lib/CashRegister.rb
+++ objects_tdd/lib/CashRegister.rb
@@ -1,5 +1,4 @@
-#!usr/bin/env ruby
-
+# cash register class
 class CashRegister
   def initialize
     @total = 0.00
@@ -8,4 +7,17 @@ class CashRegister
   def total
     format '%.2f', @total
   end
+
+  def purchase(amount)
+    @total += amount
+  end
 end
+
+
+
+puts register = CashRegister.new
+puts register.total # => 0.00
+puts register.purchase(3.78) # => 3.78
+puts register.total # => 3.78
+# puts register.pay(5.00) # => "Your change is $1.22"
+# register.total # => 0.00
--- objects_tdd/spec/lib/CashRegister_spec.rb
+++ objects_tdd/spec/lib/CashRegister_spec.rb
@@ -7,7 +7,13 @@ describe CashRegister do
   end
   describe '#total' do
     it 'should return 0 for new CashRegisters' do
-      expect(subject.total).to eq(0.00)
+      expect(subject.total).to eq('0.00')
+    end
+  end
+  describe '#purchase(amount)' do
+    it 'total should equal the amount of the purchase' do
+      subject.purchase(3.78)
+      expect(subject.total).to eq('3.78')
     end
   end
 end
