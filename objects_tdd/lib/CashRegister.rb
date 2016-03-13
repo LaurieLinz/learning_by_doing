a68c77e43aaf1d7b4baad0337b977c7557560ca3
Laurie Linz <lvlinz@comcast.net>
2016-03-12 17:15:02 -0700

    "Completed code for TDD cash register"
    [#115220675]

--- objects_tdd/lib/CashRegister.rb
+++ objects_tdd/lib/CashRegister.rb
@@ -1,4 +1,4 @@
-# cash register class
+# class
 class CashRegister
   def initialize
     @total = 0.00
@@ -11,13 +11,26 @@ class CashRegister
   def purchase(amount)
     @total += amount
   end
+
+  def pay(payment)
+    @total = payment - @total
+    puts "Your change is $#{total}"
+  end
 end
-
-
 
 puts register = CashRegister.new
 puts register.total # => 0.00
 puts register.purchase(3.78) # => 3.78
 puts register.total # => 3.78
-# puts register.pay(5.00) # => "Your change is $1.22"
-# register.total # => 0.00
+puts register.pay(5.00) # => "Your change is $1.22"
+register.total # => 0.00
+
+puts register = CashRegister.new
+puts register.total # => 0.00
+puts register.purchase(3.78) # => 3.78
+puts register.purchase(5.22) # => 9.00
+puts register.total # => 9.00
+puts register.pay(5.00) # => "Your new total is $4.00"
+puts register.total # => 4.00
+puts register.pay(5.00) # => "Your change is $1.00"
+register.total # => 0.00
