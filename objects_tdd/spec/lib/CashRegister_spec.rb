2aadb8b91bfb1f2e981c6af8116fdd45c6b8f0a9
Laurie Linz <lvlinz@comcast.net>
2016-03-14 14:44:45 -0600

    Removed redundant tests
    [#115220675]

--- objects_tdd/spec/lib/CashRegister_spec.rb
+++ objects_tdd/spec/lib/CashRegister_spec.rb
@@ -12,6 +12,12 @@ describe CashRegister do
     end
   end
 
+  describe '#total' do
+    it 'should not return 5 for new CashRegisters' do
+    expect(subject.total).to_not eq(5)
+    end
+  end
+
   describe '#purchase(amount)' do
     it 'total should equal the amount of the purchase' do
       subject.purchase(3.78)
@@ -23,39 +29,6 @@ describe CashRegister do
     it 'should return the amount owed or change due after the payment' do
       subject.pay(5 - 3.78)
       expect(subject.total).to eq('1.22')
-    end
-  end
-
-  describe '#total' do
-    it 'should return 0 for new CashRegisters' do
-      expect(subject.total).to eq('0.00')
-    end
-  end
-
-  describe '#purchase(amount)' do
-    it 'total should equal the amount of the purchase' do
-      subject.purchase(3.78)
-      expect(subject.total).to eq('3.78')
-    end
-  end
-
-  describe '#purchase(amount)' do
-    it 'total should equal the amount of the purchase' do
-      subject.purchase(5.22 + 3.78)
-      expect(subject.total).to eq('9.00')
-    end
-  end
-
-  describe '#pay(payment)' do
-    it 'should return the amount owed or change due after the payment' do
-      subject.pay(9.00 - 5.00)
-      expect(subject.total).to eq('4.00')
-    end
-  end
-  describe '#pay(payment)' do
-    it 'should return the amount owed or change due after the payment' do
-      subject.pay(5.00 - 4.00)
-      expect(subject.total).to eq('1.00')
     end
   end
 end
