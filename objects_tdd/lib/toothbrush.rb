b7dc34b74502c92d1780edb17f1f11d88b9ed58d
Laurie Linz <lvlinz@comcast.net>
2016-03-15 20:31:10 -0600

    Added code and tests
    [#115220691]

--- objects_tdd/lib/toothbrush.rb
+++ objects_tdd/lib/toothbrush.rb
@@ -1,6 +1,18 @@
 # run code twice per day or more as needed
 class Toothbrush
   attr_reader :toothbrush
+
+  def apply_toothpaste
+    'Toothpaste applied'
+  end
+
+  def brush_teeth
+    'brushed'
+  end
+
+  def rinse_brush
+    'rinsed'
+  end
 end
 
 
--- objects_tdd/spec/lib/toothbrush_spec.rb
+++ objects_tdd/spec/lib/toothbrush_spec.rb
@@ -6,15 +6,27 @@ describe Toothbrush do
     expect(subject).to be_a(Toothbrush)
   end
 
-
   describe 'with toothpaste' do
-    it 'should have toothpaste on it'
-    before do
-      @toothbrush = toothbrush.new(toothpaste: true)
-      @toothbrush.apply_toothpaste
+    it 'should have toothpaste on it' do
+      expect(subject.apply_toothpaste).to eq('Toothpaste applied')
+    end
+  end
+  describe 'brush teeth' do
+    it 'those teeth should be clean' do
+      expect(subject.brush_teeth).to eq('brushed')
+    end
+  end
+  describe 'rinse brush' do
+    it 'should rinse the brush' do
+      expect(subject.rinse_brush).to eq('rinsed')
     end
   end
 end
+
+
+
+
+
 
 
 
